dfda48e761f38f8d9ddb3cf25819b87d4d837e4f
Emanuel Joness <itzemanuelj@gmail.com>
2022-08-20 21:33:23 -0400

    intial commit awnsers 1 to 10

---
+++ 5.Variable_Names_Legal_and_Illegal.py
@@ -0,0 +1,60 @@
+# Type the illegal character in this variable name:
+# 2nd_address
+
+2
+
+# Type the illegal character in this variable name:
+# fourth-Player
+
+# -
+
+# Rewrite this variable name using the recommended style: customerlastname
+
+customer_last_name = "justin"
+
+# Rewrite this variable name using the recommended style:
+
+# totalaftertax
+
+total_after_tax = 10
+
+# Make up a variable name that includes a number.
+
+random_var20 = 20
+
+# Make up a variable name that includes a number.
+
+random_str = "random"
+
+# Assign a number to a variable. Make up the number and the variable name.
+
+my_var = 2
+
+# Assign the sum of two numbers to a variable. Make up the numbers and the variable name.
+
+two_num = 2 + 2
+
+# Assign one variable to another. Make up the variable names.
+
+var_two = 1
+my_var = var_two 
+
+# Divide one variable by another and assign the result to a third variable. Make up the variable names.
+var_1 = 1
+var_2 = 5
+var_3 =  var_1 / var_2
+print(var_3)
+
+# Correct the variable naming errors.
+
+# 4h="yes"
+# negative answer = "no"
+# asian_country = "China"
+# african-country = "Kenya"
+# print("ok")
+
+postive_awnser = "yes"
+negative_answer = "no"
+asian_country = "China"
+african_country = "Kenya"
+print("ok")
